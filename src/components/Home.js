652881464e6192d385def07cf84890b27b47a2a2
codedpills <codedpills@gmail.com>
2019-05-15 12:47:10 +0000

    home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return ( 
+        <div className="container">
+            <h4 className="">Home</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, aspernatur, 
+                eos ipsa deleniti modi possimus officia quod molestias temporibus aliquid 
+                ut aperiam odio illum non quibusdam quaerat fuga et atque!
+            </p>
+        </div>
+    );
+}
+ 
+export default Home;
