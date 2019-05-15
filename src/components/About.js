379879286d67b79d9f4b765d58ce5f36a41e90ca
codedpills <codedpills@gmail.com>
2019-05-15 12:46:52 +0000

    about component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return ( 
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, aspernatur, 
+                eos ipsa deleniti modi possimus officia quod molestias temporibus aliquid 
+                ut aperiam odio illum non quibusdam quaerat fuga et atque!
+            </p>
+        </div>
+    );
+}
+ 
+export default About;
