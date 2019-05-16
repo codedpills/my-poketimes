dc257ad8cd0b69f3036d23b7b7aae79fc4c6dc36
codedpills <codedpills@gmail.com>
2019-05-16 09:44:36 +0000

    changed Link to NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
@@ -7,9 +7,9 @@ const Navbar = () => {
             <div className="container">
                 <a className="brand-logo">Poke' Times</a>
                 <ul className="right">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
