cfbe214632c104a8f22dbe51c71366f57653d99c
codedpills <codedpills@gmail.com>
2019-05-22 14:43:33 +0000

    removed axios

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Post extends Component {
-    state = { 
-        post: null
-    }
-    componentDidMount() {
-        console.log(this.props);
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-            .then(res => {
-                this.setState({
-                    post: res.data
-                })
-                console.log(res.data);
-                
-            })
-    }
     render() { 
         const post = this.state.post ? (
             <div className="post">
